6a09bb4fbc577f227cdf737b7802ca69fa938209
Sefinek <92880265+sefinek24@users.noreply.github.com>
2022-01-07 01:09:20 +0100

    v1.9.1 | Poprawki w wyglądzie

--- index.js
+++ index.js
@@ -13,41 +13,33 @@ app.use(express.static('public'));
 app.engine('ejs', ejs.renderFile);
 app.set('view engine', 'ejs');
 
-/* Functions */
-const render = (file, req, res, data = {}) => {
-	res.render(path.resolve(`./templates/${file}.ejs`), data);
-	console.log(`GET ${req.path}`);
-};
+app.use(express.json());
 
+
+/* Functions */
+const render = (file, req, res, data = {}) => res.render(path.resolve(`./templates/${file}.ejs`), data);
+
+/* Next */
+app.get('*', (req, res, next) => {
+	console.log(`GET ${req.path}`);
+	next();
+});
 
 /* URLs */
-app.get('/', (req, res) => {
-	render('index', req, res, { version, lastUpdate });
-});
+app.get('/', (req, res) => render('index', req, res, { version, lastUpdate }));
 
-app.get('/server', (req, res) => {
-	render('server', req, res);
-});
+app.get('/server', (req, res) => render('server', req, res));
 
-app.get('/add', (req, res) => {
-	render('add', req, res);
-});
+app.get('/add', (req, res) => render('add', req, res));
 
-app.get('/labybot', (req, res) => {
-	render('labybot', req, res);
-});
+app.get('/labybot', (req, res) => render('labybot', req, res));
 
-app.get('/version', (req, res) => {
-	res.send(version);
-	console.log('GET /ver');
-});
+app.get('/version', (req, res) => res.json({ version: version }));
 
 
 /* Short URLs */
-app.get('/serverUrl', (req, res) => {
-	res.redirect('https://discord.gg/uV6HsqxBBC');
-	console.log('REDIRECT /serverUrl');
-});
+app.get('/serverUrl', (req, res) => res.redirect('https://discord.gg/uV6HsqxBBC'));
+app.get('/serverOld', (req, res) => res.redirect('https://discord.gg/YZmjrTgpDP'));
 
 
 /* Errors */
@@ -55,7 +47,7 @@ const renderError = (res, req, code, type, err, data = {}) => {
 	if (err) console.error(err);
 
 	const id = Math.floor(Math.random() * 999999 + 1);
-	res.status(code).render(path.resolve(`./templates/errors/${code}.ejs`), Object.assign({ code, id, url: req.originalUrl, shortUrl: req._parsedOriginalUrl.pathname, type: type ? 'POST' : 'GET', err }, data));
+	res.status(code).render(path.resolve(`./templates/errors/${code}.ejs`), Object.assign({ code, id, err, url: req.originalUrl, shortUrl: req._parsedOriginalUrl.pathname, type: type ? 'POST' : 'GET' }, data));
 
 	console.log(`WebERR » Cannot GET: ${req.originalUrl} [${code}] [${id}]`);
 };
@@ -66,9 +58,7 @@ app.use((req, res) => {
 
 app.use((err, req, res, next) => {
 	renderError(res, req, 500, false, err);
-	console.log(`Cannot GET: ${req.path} [500]\n${err}`);
-
 	return next;
 });
 
-app.listen(port, null, null, () => console.log(`Strona https://skiffybot.xyz została uruchomiona na porcie ${port}.`));
+app.listen(port, null, null, () => console.log(`Strona https://skiffybot.xyz została uruchomiona na http://127.0.0.1:${port}.`));
