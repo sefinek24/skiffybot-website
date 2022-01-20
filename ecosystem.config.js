692a860bf9b133300ff46ab5ac34477890a688ec
Sefinek <92880265+sefinek24@users.noreply.github.com>
2022-01-20 13:18:55 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,11 @@ module.exports = {
 		log_date_format : 'HH:mm:ss, DD.MM.YYYY',
 		error_file      : '/home/ubuntu/logs/www/main/error.log',
 		out_file        : '/home/ubuntu/logs/www/main/out.log',
+
+		max_restarts          : 10,
+		restart_delay         : 6000,
+		shutdown_with_message : true,
+		wait_ready            : true,
+		listen_timeout        : 35000,
 	}],
-
-	max_restarts: 10,
-	restart_delay: 6000,
-	shutdown_with_message: true,
-	wait_ready: true,
-
-	env_production: { NODE_ENV: 'production' },
 };
