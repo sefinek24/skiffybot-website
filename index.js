47451d8bd9be55c02d06cd1479eda5bc7f5cad49
Sefinek <92880265+sefinek24@users.noreply.github.com>
2022-05-07 09:25:28 +0200

    Deleted test

--- index.js
+++ index.js
@@ -23,10 +23,6 @@ app.get('/server', (req, res) => res.render('server'));
 app.get('/add', (req, res) => res.render('add'));
 app.get('/datadog', (req, res) => res.render('datadog'));
 app.get('/version', (req, res) => res.json({ version }));
-app.get('/test555', (req, res) => {
-	console.log(req.headers['x-forwarded-for'] || req.socket.remoteAddress);
-	res.send('OK');
-});
 
 // Links
 app.get('/support', (req, res) => res.redirect('https://discord.gg/YZmjrTgpDP'));
