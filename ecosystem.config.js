00e4a06d02c1eda883676f27eea40dc007b083b2
Sefinek <92880265+sefinek24@users.noreply.github.com>
2022-06-07 16:01:56 +0200

    Aktualizacja configu

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,14 @@ module.exports = {
 	apps: [{
 		name            : 'skiffybot-website',
 		script          : './index.js',
+
 		log_date_format : 'HH:mm:ss, DD.MM.YYYY',
 		error_file      : '/home/ubuntu/logs/www/main/error.log',
 		out_file        : '/home/ubuntu/logs/www/main/out.log',
 
-		max_restarts          : 10,
-		restart_delay         : 6000,
+		max_restarts          : 9,
+		restart_delay         : 2000,
 		shutdown_with_message : true,
 		wait_ready            : true,
-		listen_timeout        : 10000,
 	}],
 };
